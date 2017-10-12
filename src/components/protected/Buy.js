de422148c77caaa735c84c06481d9ff4b0f775c7
Ram <irraju@gmail.com>
2017-10-11 20:18:55 -0500

    Added Fake Cart

--- src/components/protected/Buy.js
+++ src/components/protected/Buy.js
@@ -1,26 +1,59 @@
 import React, { Component } from 'react'
-import { Grid, Segment } from 'semantic-ui-react'
+import { Grid, Segment, Statistic, Card } from 'semantic-ui-react'
 
 
 export default class Cart extends Component {
+
+
+  /**
+  Car shows up only if there items added
+  */
+
   render() {
     return (
       <div className="buy head">
-        <h1>Products and Cart</h1>
+        <h1>Products</h1>
         <Grid>
-          <Grid.Row className="cart">
-            <Grid.Column>
-              <Segment>{`Items added to cart show up here`}</Segment>
-            </Grid.Column>
-          </Grid.Row>
           <Grid.Row className="products">
             <Grid.Column>
               <Segment>{`All products and categories show up here`}</Segment>
             </Grid.Column>
           </Grid.Row>
         </Grid>
+        <div className="cart">
+          <Segment>
+            { this.renderCartSummary() }
+            { this.renderCartItems() }
+          </Segment>
+        </div>
       </div>
     )
+  }
+
+  renderCartSummary() {
+    //dummy data
+    const items = [
+      { label: 'Items', value: '5' },
+      { label: 'Tons', value: '31.4' },
+      { label: 'Price', value: '1,40,000.00' },
+    ];
+    return (
+      <div className="summary">
+        <Statistic.Group items={items} size='mini' />
+      </div>
+    )
+  }
+
+  renderCartItems() {
+    const src = ''
+    return(
+      <Card.Group>
+        <Card color='red' inverted/>
+        <Card color='orange' inverted/>
+        <Card color='yellow' inverted/>
+        <Card color='green' inverted/>
+      </Card.Group>
+    );
   }
 
 
